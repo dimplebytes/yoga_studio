664d922a68dece3eb1ba9fdfd92d2874408776ea
Dimple <dimple.tumkur@gmail.com>
2023-03-04 19:53:58 +0530

    style add

--- index.php
+++ index.php
@@ -7,46 +7,64 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=neon">
   <title>Yoga Studio</title>
 
 </head>
 <body>
-<h1>YOGA Studio</h1>
-</head>
+  
 
 <div class="Main">
-		<div class="form-box">
-			<div class="button-box">
-				<div id="btn"></div>
-				<button type="button" class="toggle-btn" onclick="login()">Login</button>
-				<button type="button" class="toggle-btn" onclick="register()">Register</button>
+ 
+<div class="header-div">
+				<div class="logo-div" id="logo-div"><h1>YOGA-Studio</h1></div>
+				<div class="button-box" id="button-box">
+				<button type="button" class="toggle-btn" onclick="showLogin()">Login</button>
+				<button type="button" class="toggle-btn" onclick="showRegister()">Register</button>
+                </div>
 			</div>
 			    
+		<div class="form-box">
+			
 		    	
-		    	<!--<form id="login" class="input-group">
-		    		<input type="text" class="input-field" placeholder="User Id" required>
-		    		<input type="text" class="input-field" placeholder="Enter-Password" required>
-		    		<input type="checkbox" class="check-box"><span>remember password</span>
+		    	<div class="login-div" id="login-div" style="display:none;">
+					<form id="login" class="input-group">
+		    		<input type="text" class="input-field" placeholder="User Id" required><br>
+		    		<input type="text" class="input-field" placeholder="Enter-Password" required><br>
+		    		<input type="checkbox" class="check-box"><span>remember password</span><br>
 		    		<button type="submit" class="submit-btn">Login In</button>
-		    	</form>-->
- <div class="register-div">
-
-		    	<form id="register" class="input-group">
+		    		</form>
+				</div>
+ 				<div class="register-div" id="register-div" style="display:none;">
+					<form id="register" class="input-group">
 		    		<input type="text" class="input-field" placeholder="User Id" required><br>
 		    		<input type="email" class="input-field" placeholder="Email Id" required><br>
 		    		<input type="text" class="input-field" placeholder="Enter password" required><br>
 		    		<input type="checkbox" class="check-box"><span>I Agree to The Term & Condition</span><br>
 		    		<button type="submit" class="submit-btn">Register</button>
-		    	</form>
-</div>
+		    		</form>
+				</div>
 		</div>
 	</div>
-	
+	<script>
+function showLogin() {
+  var x = document.getElementById("login-div");
+  var y = document.getElementById("register-div");
+   x.style.display = "flex";
+   y.style.display = "none";
+}
+  
+
+
+	function showRegister() {
+  var x = document.getElementById("login-div");
+  var y = document.getElementById("register-div");
+   x.style.display = "none";
+   y.style.display = "flex";
+	}
+  
+</script> 
 </body>
-
-
-</body>
-
 </html>
 
 
